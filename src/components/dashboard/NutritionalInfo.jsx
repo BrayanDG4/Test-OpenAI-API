d0430157415fb53f6f58951b1dd68bb445ce8f5d
Brayan Gomez <brayandgomez4@gmail.com>
2023-05-29 15:44:43 -0500

    Integrando cambios

--- src/components/dashboard/NutritionalInfo.jsx
+++ src/components/dashboard/NutritionalInfo.jsx
@@ -12,32 +12,46 @@ export const NutritionalInfo = () => {
           </h1>
         </div>
 
-        <div className="flex justify-start my-4 gap-5 text-xl font-semibold my-title-color">
-          <div>
-            <h3>Género: {genero || ""}</h3>
+        <div className="border-solid border-2 border-gray-200 rounded-md py-6 px-2 bg-[#EDCF43]">
+          <div className="flex justify-start text-3xl font-bold my-title-color mb-2">
+            <h3>Hola, estos son tus datos!</h3>
           </div>
+          <div className="flex justify-start gap-5 text-xl font-semibold my-title-color">
+            <div>
+              <h3>Género: {genero || ""}</h3>
+            </div>
 
-          <div>
-            <h3>Enfermedad: {enfermedad || ""}</h3>
+            <div>
+              <h3>Enfermedad: {enfermedad || ""}</h3>
+            </div>
+
+            <div>
+              <h3>Edad: {edad || ""}</h3>
+            </div>
+
+            <div>
+              <h3>Estatura: {estatura || ""}</h3>
+            </div>
+
+            <div>
+              <h3>Peso: {peso || ""}</h3>
+            </div>
           </div>
+        </div>
 
-          <div>
-            <h3>Edad: {edad || ""}</h3>
+        <div className="w-full flex justify-center gap-4 bg-blue-500 rounded-md">
+          <div className="w-[50%] bg-red-500">
+            <h3>HOLA</h3>
           </div>
-
-          <div>
-            <h3>Estatura: {estatura || ""}</h3>
-          </div>
-
-          <div>
-            <h3>Peso: {peso || ""}</h3>
+          <div className="w-[50%] bg-purple-700">
+            <h3>HOLA</h3>
           </div>
         </div>
 
         <div>
           <Card />
         </div>
-        
+
         <div className="text-start">
           <p className="bg-[#dddddd] text-[#666666] p-2 rounded-md outline-none border">
            {respuesta}
